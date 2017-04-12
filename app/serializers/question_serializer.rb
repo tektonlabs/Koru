0df6cfef9520f53d898014d6cee4c7468f2dbbd9
jc3000 <jorge.calvo@tektonlabs.com>
2017-04-12 09:41:11 -0500

    adding min and max values for questions

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,6 +1,9 @@
 class QuestionSerializer < ActiveModel::Serializer
 
   attributes :id, :text, :answers, :question_type, :sub_questions
+  attribute :min_text, key: :min_value
+  attribute :max_text, key: :max_value
+
   
   def sub_questions
     object.sub_questions.map do |sub_question|
--- app/serializers/sub_question_serializer.rb
+++ app/serializers/sub_question_serializer.rb
@@ -1,5 +1,7 @@
 class SubQuestionSerializer < ActiveModel::Serializer
 
   attributes :id, :text, :answers, :question_type
+  attribute :min_text, key: :min_value
+  attribute :max_text, key: :max_value
 
 end
