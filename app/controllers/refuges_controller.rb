0ef0a949275c21efcca32d81512c21cfa3a770af
jc3000 <jorge.calvo@tektonlabs.com>
2017-04-18 11:17:26 -0500

    adding partials for navbars

--- app/controllers/refuges_controller.rb
+++ app/controllers/refuges_controller.rb
@@ -1,11 +1,17 @@
 class RefugesController < ApplicationController
 
+  before_action :set_refuge, only: :detail
+
   def index
   end
 
   def detail
+  end
+
+  private
+
+  def set_refuge
     @refuge = Refuge.find params[:id]
-    ap 'hola me llamo jorgito'
   end
 
 end
