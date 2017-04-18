0cfa67b1c7b67492be8041edd00c9dd1dcb72bd2
jc3000 <jorge.calvo@tektonlabs.com>
2017-04-18 13:09:18 -0500

    change name

--- db/migrate/20170410202829_create_refuges.rb
+++ db/migrate/20170410202829_create_refuges.rb
@@ -2,8 +2,8 @@ class CreateRefuges < ActiveRecord::Migration[5.0]
   def change
     create_table :refuges do |t|
       t.string :name, null: false
-      t.string :lat
-      t.string :long
+      t.string :latitude
+      t.string :longitude
 
       t.timestamps
     end
