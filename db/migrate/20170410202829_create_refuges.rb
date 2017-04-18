b25a941420deedc90e5580bfa9968cf9ffeaa9c7
jc3000 <jorge.calvo@tektonlabs.com>
2017-04-18 12:28:29 -0500

    adding new refuges

--- db/migrate/20170410202829_create_refuges.rb
+++ db/migrate/20170410202829_create_refuges.rb
@@ -2,6 +2,8 @@ class CreateRefuges < ActiveRecord::Migration[5.0]
   def change
     create_table :refuges do |t|
       t.string :name, null: false
+      t.string :lat
+      t.string :long
 
       t.timestamps
     end
--- db/seeds/02_refuge.seeds.rb
+++ db/seeds/02_refuge.seeds.rb
@@ -1,3 +1,14 @@
-refuge = Refuge.create name: "Piura"
+refuge = Refuge.create name: "Cristo Viene", lat: "-5.3252875", long: "-80.6641267"
   RefugeEntity.create refuge: refuge, entity: Entity.find_by(name: "Limpieza")
   RefugeEntity.create refuge: refuge, entity: Entity.find_by(name: "Luz")
+
+Refuge.create name: "Jesús de Nazareth", lat: "-5.3251123", long: "-80.6644547"
+Refuge.create name: "Santa Rosa", lat: "-5.3251123", long: "-80.6644547"
+Refuge.create name: "Túpac Amaru", lat: "-5.3251123", long: "-80.6644547"
+Refuge.create name: "San Martín", lat: "-5.3251123", long: "-80.6644547"
+Refuge.create name: "Juan Pablo II - La Legua", lat: "-5.2371187", long: "-80.6724573"
+Refuge.create name: "Villa San Jacinto Casa de retiro", lat: "-5.2467128", long: "-80.6721022"
+Refuge.create name: "Casa María", lat: "-5.2007787", long: "-80.6327704"
+Refuge.create name: "Casa Comunal AA.HH. Fátima", lat: "-5.1941916", long: "-80.6535585"
+Refuge.create name: "Colegio San José", lat: "-5.1806674", long: "-80.6534903"
+Refuge.create name: "Colegio San Ignacio de Loyala", lat: "-5.1869505", long: "-80.62148"
