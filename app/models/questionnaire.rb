74355ee4828c76837eb9f34f19ef2202b2467ba9
jc3000 <jorge.calvo@tektonlabs.com>
2017-04-28 17:34:08 -0500

    adding doughnut functionality

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -4,6 +4,7 @@ class Questionnaire < ApplicationRecord
   has_many :responses
 
   def save_with_responses questions_params, date_param
+    self.refuge.refuge_entities.update_all issues_number: 0
     self.state_date = Time.at date_param.to_i
     questions_params.each do |question|
       unless question[:sub_questions].blank?
@@ -27,10 +28,22 @@ class Questionnaire < ApplicationRecord
       case question_param[:question_type].to_sym
       when :one_choice
         unless answers[0][:selected_id].blank?
+          if answers[0][:selected_id] != Answer.find_by(name: "Sí").id
+            refuge_entity = RefugeEntity.find_by(refuge_id: self.refuge_id, entity: Question.find(question_param[:id]).entity)
+            if refuge_entity
+              refuge_entity.issues_number += 1
+              refuge_entity.save
+            end
+          end
           self.responses.build(question_id: question_param[:id], answer_selected_id: [answers[0][:selected_id]])
         end
       when :multiple_choice
         if answers.map{|a| a[:selected_id]}
+          refuge_entity = RefugeEntity.find_by(refuge_id: self.refuge_id, entity: Question.find(question_param[:id]).entity)
+          if refuge_entity
+            refuge_entity.issues_number += 1
+            refuge_entity.save
+          end
           self.responses.build(question_id: question_param[:id], answer_selected_id: answers.map{|a| a[:selected_id]})
         end
       when :input_value
