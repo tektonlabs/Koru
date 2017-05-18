bb0befa07189c42ca0e900c3002c0cb42034cca6
Eduardo Arenas <eduardo.arenas@tektonlabs.com>
2017-05-18 15:23:47 +0000

    answer.rb edited online with Bitbucket

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,7 +6,8 @@ class Answer < ApplicationRecord
   has_many :questions, through: :question_answers
 
   def self.text_answers_selected answers_array, other_value
-    other_value.blank? ? Answer.where(id: answers_array).map(&:name).join(", ") : (Answer.where(id: answers_array).map(&:name).join(", ") + "(" + other_value + ")")
+    #other_value.blank? ? Answer.where(id: answers_array).map(&:name).join(", ") : (Answer.where(id: answers_array).map(&:name).join(", ") + "(" + other_value + ")")
+    "#{Answer.where(id: answers_array).map(&:name).join(", ")}#{" (#{other_value})" unless other_value.blank?}"
   end
 
 end
