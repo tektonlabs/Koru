ab7e44f792f1b5debbcf0be78a186e0babb2c09c
Eduardo Arenas <eduardo.arenas@tektonlabs.com>
2017-06-01 16:31:52 +0000

    refuge.rb edited online with Bitbucket

--- app/models/refuge.rb
+++ app/models/refuge.rb
@@ -41,7 +41,7 @@ class Refuge < ApplicationRecord
   end
 
   def observation_responses
-    self.last_questionnaire.nil? ? nil : self.last_questionnaire.responses.joins(:question).where('questions.question_type = 2 AND questions.text != ? AND questions.text != ?', 'Â¿Por quÃ©?', 'Â¿QuiÃ©n es el encargado del recojo de basura?')
+    self.last_questionnaire.nil? ? nil : self.last_questionnaire.responses.joins(:question).where('questions.question_type = 2 AND questions.text != ? AND questions.text != ?', '¿Por qué?', '¿Quién es el encargado del recojo de basura?')
   end
 
   def set_status
@@ -108,18 +108,16 @@ class Refuge < ApplicationRecord
   end
 
   def self.filter_by_entity entity_id = nil, query = nil
-    refuges = nil
-    unless entity_id.nil?
+    return Refuge.all if (entity_id.blank? and query.blank?)
+    refuges = []
+    if !entity_id.blank?
       refuge_entities = RefugeEntity.where("entity_id IN (?) AND issues_number != 0", entity_id)
       refuges = Refuge.where(id: refuge_entities.map(&:refuge_id))
-    else
-      refuges = Refuge.all
     end
-    unless query.nil?
+    if !query.blank?
       refuges.joins(:country).where("refuges.name ILIKE ? OR refuges.city ILIKE ? OR refuges.address ILIKE ? OR countries.name ILIKE ?", "%#{query}%", "%#{query}%", "%#{query}%", "%#{query}%")
-    else
-      refuges
     end
+    return refuges
   end
 
 end
