8a336435983410dac446249383ae0056e5543814
jc3000 <jorge.calvo@tektonlabs.com>
2017-06-02 15:05:30 -0500

    remove filter and search bar on refuge detail page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,14 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  before_action :set_controller
+  before_action :set_controller, :set_action
 
   def set_controller
     @controller = params[:controller]
+  end
+
+  def set_action
+    @action = action_name
   end
 
 end
