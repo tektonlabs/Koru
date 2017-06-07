dc0a748f98ded87a0f66f32bf29a40dde74a6c41
jc3000 <jorge.calvo@tektonlabs.com>
2017-06-07 15:50:30 -0500

    refactor configurations

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,5 @@ Bundler.require(*Rails.groups)
 
 module RefugiosApp
   class Application < Rails::Application
-
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :es
-
   end
 end
