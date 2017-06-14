a321709dddc9e31d536c0bcb27930eb76ad2be43
jc3000 <jorge.calvo@tektonlabs.com>
2017-06-14 14:13:09 -0500

    fix orthography

--- db/seeds/09_areas.seeds.rb
+++ db/seeds/09_areas.seeds.rb
@@ -1,4 +1,4 @@
 Area.create name: "Tópico"
 Area.create name: "Área de recreación"
 Area.create name: "Área para cocinar"
-Area.create name: "Almacen"
+Area.create name: "Almacén"
--- db/seeds/10_light_management.seeds.rb
+++ db/seeds/10_light_management.seeds.rb
@@ -1,4 +1,4 @@
 LightManagement.create name: "Red de luz pública"
 LightManagement.create name: "Alumbrado público"
-LightManagement.create name: "Motor (generador)"
+LightManagement.create name: "Motor (Generador)"
 LightManagement.create name: "Velas / Lamparines"
