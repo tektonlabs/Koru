028fac4c0f234bf94209a1b8d9a5d954a89e36ac
jc3000 <jorge.calvo@tektonlabs.com>
2017-06-22 10:13:46 -0500

    adding new fields to refuges

---
+++ db/migrate/20170622141332_add_new_fields_to_refuges.rb
@@ -0,0 +1,15 @@
+class AddNewFieldsToRefuges < ActiveRecord::Migration[5.0]
+  def change
+    add_column :refuges, :children_under_3_male, :integer, null: false, default: 0
+    add_column :refuges, :children_under_3_female, :integer, null: false, default: 0
+    add_column :refuges, :people_less_than_or_equals_to_18_male, :integer, null: false, default: 0
+    add_column :refuges, :people_less_than_or_equals_to_18_female, :integer, null: false, default: 0
+    add_column :refuges, :people_older_than_18_male, :integer, null: false, default: 0
+    add_column :refuges, :people_older_than_18_female, :integer, null: false, default: 0
+    add_column :refuges, :older_adults_male, :integer, null: false, default: 0
+    add_column :refuges, :older_adults_female, :integer, null: false, default: 0
+    remove_column :refuges, :number_of_people, :integer
+    remove_column :refuges, :number_of_children_under_3, :integer
+    remove_column :refuges, :number_of_older_adults, :integer
+  end
+end
