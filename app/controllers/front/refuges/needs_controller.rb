673ed0578deccf2e9f87fd4342e5730bd3aa166f
jc3000 <jorge.calvo@tektonlabs.com>
2017-07-20 14:39:03 -0500

    fix indentation

--- app/controllers/front/refuges/needs_controller.rb
+++ app/controllers/front/refuges/needs_controller.rb
@@ -1,19 +1,19 @@
 class Front::Refuges::NeedsController < FrontController
 
-	before_action :set_refuge, only: :assign
-	before_action :set_need, only: :assign
+  before_action :set_refuge, only: :assign
+  before_action :set_need, only: :assign
 
-	def assign
-	end
+  def assign
+  end
 
-	private
+  private
 
-	def set_refuge
-		@refuge = Refuge.find params[:refuge_id]
-	end
+  def set_refuge
+    @refuge = Refuge.find params[:refuge_id]
+  end
 
-	def set_need
-		@need = Need.find params[:id]
-	end
+  def set_need
+    @need = Need.find params[:id]
+  end
 
 end
