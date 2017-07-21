f2b6e4b89ace3507542647aae3caf3fde540f473
jc3000 <jorge.calvo@tektonlabs.com>
2017-07-21 16:53:17 -0500

    fix

--- app/helpers/front_refuges_helper.rb
+++ app/helpers/front_refuges_helper.rb
@@ -104,12 +104,8 @@ module FrontRefugesHelper
   end
 
   def hide_sidebar_content_if controller, action
-    forbidden_controllers = []
-    forbidden_actions = []
-    forbidden_controllers << "front/refuges"
-    forbidden_controllers << "front/refuges/needs"
-    forbidden_actions << "assign"
-    forbidden_actions << "show"
+    forbidden_controllers = ["front/refuges/needs","front/refuges"]
+    forbidden_actions = ["show","assign"]
     forbidden_controllers.include?(controller) and forbidden_actions.include?(action)
   end
 
