b00b13ed8c15454bb8b0c9e5b0c00fcfa1b1c983
jorgitotk <jorge.calvo@tektonlabs.com>
2017-10-11 10:24:28 -0500

    Adding licence comment on all files

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -1,7 +1,21 @@
-# Be sure to restart your server when you modify this file.
+# <!--
 
-# You can add backtrace silencers for libraries that you're using but don't wish to see in your backtraces.
-# Rails.backtrace_cleaner.add_silencer { |line| line =~ /my_noisy_library/ }
+# =========================================================================== 
+# Koru GPL Source Code 
+# Copyright (C) 2017 Tekton Labs
+# This file is part of the Koru GPL Source Code.
+# Koru Source Code is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published by 
+# the Free Software Foundation, either version 3 of the License, or 
+# (at your option) any later version. 
 
-# You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
-# Rails.backtrace_cleaner.remove_silencers!
+# Koru Source Code is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# GNU General Public License for more details. 
+
+# You should have received a copy of the GNU General Public License 
+# along with Koru Source Code. If not, see <http://www.gnu.org/licenses/>. 
+# =========================================================================== 
+
+# */
